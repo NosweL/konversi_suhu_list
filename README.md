592980d8b265f44b7ffb73c17ebe55b3fc61f529
noswel <noswelvr@gmail.com>
2022-11-21 15:22:56 +0700

    Konversi Suhu List

---
+++ README.md
@@ -0,0 +1,6 @@
+# konversi_suhu
+##Screenshot
+RAW
+![convert suhu1](https://user-images.githubusercontent.com/74538482/201578954-f0edd5dd-3c4e-4bd8-9b54-ac4b63ac4554.jpeg)
+Input Number
+![convert suhu2](https://user-images.githubusercontent.com/74538482/201579006-5e3bbf01-d6df-448a-ae65-36044cb08346.jpeg)
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'convert.dart';
+import 'dropdown.dart';
+import 'input.dart';
+import 'result.dart';
+import 'history.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+// This widget is the root of your application.
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController etInput = TextEditingController();
+
+  double _inputUser = 0;
+  var listItem = ["Kelvin", "Reamur"];
+  String _newValue = "Kelvin";
+  double _result = 0;
+  List<String> listViewItem = [];
+
+  void perhitunganSuhu() {
+    setState(() {
+      _inputUser = double.parse(etInput.text);
+      if (_newValue == "Kelvin") {
+        _result = _inputUser + 273;
+      } else {
+        _result = (4 / 5) * _inputUser;
+      }
+
+      addItemToList();
+    });
+  }
+
+  void addItemToList() {
+    setState(() {
+      String value = "$_newValue : ${_result.toStringAsFixed(1)}";
+      listViewItem.insert(0, value);
+    });
+  }
+
+  void dropdownOnChanged(String? changeValue) {
+    setState(() {
+      _newValue = changeValue!;
+      perhitunganSuhu();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Temperature Converter - 2041720253 Welson Mario Naibaho"),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Input(etInput: etInput),
+              DropdownList(
+                  listItem: listItem,
+                  newValue: _newValue,
+                  dropdownOnChanged: dropdownOnChanged),
+              Result(result: _result),
+              Convert(konvertHandler: perhitunganSuhu),
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                child: const Text(
+                  "Conversion History",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(child: HistoryView(listViewItem: listViewItem))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
